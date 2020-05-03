3d5957e10379d2ab9d760fcadb5c3a80ad331217
emanuks <emanuels313@gmail.com>
2020-05-03 00:43:00 -0300

    web scraping amazon

--- utils/GetQuestionsAmazon.py
+++ utils/GetQuestionsAmazon.py
@@ -4,25 +4,23 @@ html = open('../arquivos/amazon.html')
 soup = bs4.BeautifulSoup(html, 'html.parser')
 
 lista = []
-testes = soup.findAll('div',{'class':'a-fixed-left-grid a-spacing-small'})
-print(len(testes))
-for teste in testes:
-    pergunta = teste.find("span", 'a-declarative')
-    print(pergunta.text)
+respostas = []
+perguntas = []
 
-testes2 = soup.findAll('div', {'class': 'a-fixed-left-grid-inner'})
-print(len(testes2))
-for teste in testes2:
-    pergunta = teste.find("span", '')
-    if pergunta:
-        print(pergunta.text)
+# Obtem as perguntas
+question_items = soup.findAll('div', {'class': 'a-fixed-left-grid a-spacing-small'})
+for question_item in question_items:
+    if question_item:
+        perguntas.append(question_item.find("span", 'a-declarative'))
 
-#perguntas = soup.findNext('div',{'class':'a-fixed-left-grid a-spacing-small'}).findAll("span", 'a-declarative')
-# perguntas = soup.findAll("span", {"class": ""})
-# respostas = soup.findAll("span", {"class": "ui-pdp-color--BLACK ui-pdp-size--SMALL ui-pdp-family--REGULAR ui-pdp-qadb__questions-list__answer-item__answer"})
-#or pergunta in perguntas:
-#    print(pergunta.text)
-#for i in range(0, len(perguntas)):
-#    lista.append({"pergunta": perguntas[i].text, "respostas": respostas[i].text})
+# Obtem as respostas
+answer_items = soup.findAll('div', {'class': 'a-fixed-left-grid-col a-col-right'})
+for answer_item in answer_items:
+    resposta = answer_item.find("span", '')
+    if resposta and not(resposta in respostas):
+        respostas.append(resposta)
+
+for i in range(0, len(perguntas)):
+    lista.append({"pergunta": perguntas[i].text.strip(), "respostas": respostas[i].text})
 
 print(lista)
