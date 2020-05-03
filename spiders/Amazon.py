dc9a7ac037b610e9a2a85e869e5193c2bfae696b
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2020-05-03 16:06:07 -0300

    Melhorias Amazon Code

--- spiders/Amazon.py
+++ spiders/Amazon.py
@@ -1,13 +1,30 @@
 import scrapy
 
+
 class HermesSpiderAmazon(scrapy.Spider):
     name = "HermesSpiderAmazon"
     start_urls = ["https://www.amazon.com.br/ask/questions/asin/B001E5MO5E"]
     download_delay = 2.0
+    cont = 0
 
     def parse(self, response):
-        filename = 'arquivos/amazon.html'
-
+        next_page = []
+        filename = 'arquivos/amazon-%s.html' % self.cont
+        self.cont += 1
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+        # Seleciona todos os link que tem oferta de dados no nome
+        NEXT_PAGE_SELECTOR_A = 'a::attr(href)'
+        NEXT_PAGE_SELECTOR = '//li[has-class("a-last")]'
+
+        response_href = response.xpath(NEXT_PAGE_SELECTOR).css(NEXT_PAGE_SELECTOR_A).extract()
+
+        print(response_href)
+        # Adicionando a url inicial nos links obtidos
+        next_page.append("https://www.amazon.com.br/" + response_href[0])
+
+        # Para todas as urls
+        for page in next_page:
+            yield scrapy.http.Request(page, callback=self.parse)
--- utils/GetQuestionsAmazon.py
+++ utils/GetQuestionsAmazon.py
@@ -1,26 +1,47 @@
 import bs4
 
-html = open('../arquivos/amazon.html')
-soup = bs4.BeautifulSoup(html, 'html.parser')
+def get_faq(soup):
+    lista = []
 
-lista = []
-respostas = []
-perguntas = []
+    respostas = []
+    perguntas = []
 
-# Obtem as perguntas
-question_items = soup.findAll('div', {'class': 'a-fixed-left-grid a-spacing-small'})
-for question_item in question_items:
-    if question_item:
-        perguntas.append(question_item.find("span", 'a-declarative'))
+    # Obtem as perguntas
+    question_items = soup.findAll('div', {'class': 'a-fixed-left-grid a-spacing-small'})
+    for question_item in question_items:
+        if question_item:
+            perguntas.append(question_item.find("span", 'a-declarative'))
 
-# Obtem as respostas
-answer_items = soup.findAll('div', {'class': 'a-fixed-left-grid-col a-col-right'})
-for answer_item in answer_items:
-    resposta = answer_item.find("span", '')
-    if resposta and not(resposta in respostas):
-        respostas.append(resposta)
+    # Obtem as respostas
+    answer_items = soup.findAll('div', {'class': 'a-fixed-left-grid-col a-col-right'})
+    for answer_item in answer_items:
+        resposta = answer_item.find("span", '')
+        if resposta and not (resposta in respostas):
+            respostas.append(resposta)
 
-for i in range(0, len(perguntas)):
-    lista.append({"pergunta": perguntas[i].text.strip(), "respostas": respostas[i].text})
+    for i in range(0, len(perguntas)):
+        lista.append(
+            {
+             "pergunta": perguntas[i].text.strip(),
+             "respostas": respostas[i].text if i < len(respostas) else ""
+            }
+        )
 
-print(lista)
+    return lista
+
+
+if __name__ == '__main__':
+    from os import walk
+    directory = '../arquivos/'
+    faq_list = []
+    cont = 0
+    for (dirpath, dirnames, filenames) in walk(directory):
+        for filename in filenames:
+            html = open(directory + filename)
+            soup = bs4.BeautifulSoup(html, 'html.parser')
+            faq = get_faq(soup)
+            faq_list.append(faq)
+    for faq_li in faq_list:
+        cont += len(faq_li)
+    print(faq_list)
+    print(cont)
