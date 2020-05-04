95dc7363730ce4219be75ffbd4437b29bf9d8ca9
JoaoBispo110 <joaoguibis@hotmail.com>
2020-05-04 19:52:19 -0300

    Terminei o preprocassamento do treinamento

--- Modules/PreprocessamentoTreinamentoAI.py
+++ Modules/PreprocessamentoTreinamentoAI.py
@@ -2,8 +2,6 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import spacy
 import fasttext
-#import base
-from utils.GetQuestionsAmazon import run_amazon
 
 def InicializarAI():
 	ft_model = fasttext.load_model('cc.pt.50.bin')
@@ -11,14 +9,36 @@ def InicializarAI():
 
 	return [spacy_nlp, ft_model]
 
-def Treinamento(lista_pares_perguntas_resultado, spacy_nlp, ft_model):
+def Treinamento(spacy_nlp, ft_model):
+	i = 1
 	embed1 = []
 	embed2 = []
-	fuzz = []
+	fuzzylist = []
 	spacy = []
 	y = []
+	par_pergunta_resultado = {}
+	lista_pares_perguntas_resultado = []
+
+	arq = open('base_de_treino.txt', 'r')
+
+	while(i < 50):
+		par_pergunta_resultado['pergunta1'] = arq.readline()
+		par_pergunta_resultado['pergunta1'] = par_pergunta_resultado['pergunta1'].rstrip('\n')
+
+		par_pergunta_resultado['pergunta2'] = arq.readline()
+		par_pergunta_resultado['pergunta2'] = par_pergunta_resultado['pergunta2'].rstrip('\n')
+
+		par_pergunta_resultado['resultado'] = arq.readline()
+		par_pergunta_resultado['resultado'] = par_pergunta_resultado['resultado'].rstrip('\n')
+
+		lista_pares_perguntas_resultado.append({'pergunta1' : par_pergunta_resultado['pergunta1'], 'pergunta2' : par_pergunta_resultado['pergunta2'], 'resultado' : par_pergunta_resultado['resultado']})
+
+		i = i + 1
+
+	arq.close()
 
 	for par_pergunta_resultado in lista_pares_perguntas_resultado:
+
 		pergunta1 = par_pergunta_resultado['pergunta1']
 		pergunta2 = par_pergunta_resultado['pergunta2']
 		resultado = par_pergunta_resultado['resultado']
@@ -27,7 +47,7 @@ def Treinamento(lista_pares_perguntas_resultado, spacy_nlp, ft_model):
 
 		numero_fuzzy = fuzz.partial_ratio(pergunta1, pergunta2)
 
-		fuzz.append(numero_fuzzy)
+		fuzzylist.append(numero_fuzzy)
 
 		spacy_doc1 = spacy_nlp(pergunta1)
 		spacy_doc2 = spacy_nlp(pergunta2)
@@ -41,4 +61,4 @@ def Treinamento(lista_pares_perguntas_resultado, spacy_nlp, ft_model):
 		vetor_pergunta2 = ft_model.get_sentence_vector(pergunta2)
 		embed2.append(vetor_pergunta2)
 
-		return [embed1, embed2, fuzz, spacy, y]
+	return [embed1, embed2, fuzzylist, spacy, y]
