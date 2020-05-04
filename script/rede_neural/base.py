27f08c3ed0a1b7a59ce8cda9f5f092074ea3f8f4
guimendel <guimendeln@gmail.com>
2020-05-03 23:41:40 -0300

    criada uma função que estima diferentes custos com valores diferentes de graus de combinações entre as entradas e coeficientes de regularização (lambdas)

--- script/rede_neural/base.py
+++ script/rede_neural/base.py
@@ -4,9 +4,21 @@ from scipy import optimize as opt
 
 def obterCombinacoes(a, b, n):
     ret = np.array([[]])
+
+    # print(type(a))
+    if type(a) != np.ndarray:
+        combinar = lambda a,b,i,m : np.array([[a**i * b**(m-i)]])
+    else:
+        combinar = lambda a,b,i,m : a**i * b**(m-i)
+    
     for m in (np.array(range(n))+1):
         for i in range(m+1):
-            ret = np.concatenate((ret, [[a**i * b**(m-i)]]), axis=1)
+            aux = combinar(a, b, i, m)
+            if m==1 and i==0:
+                ret = aux
+            else:
+                # print(ret.shape, aux.shape)
+                ret = np.concatenate((ret, aux), axis=1)
     return ret
 
 def gerarInputAleatorio(m, elements, grau):
@@ -27,6 +39,21 @@ def gerarInputAleatorio(m, elements, grau):
         else:
             ret = np.concatenate((ret, case), axis=0)
     return ret
+
+# retorna 3 matrizes de exemplos: os exemplos de treino, de cross validation, e de teste
+# fractions é uma tupla que determina em quais fracoes deve ser dividido os exemplos (exmplo: (6, 2, 2))
+def dividirExemplos(X, fractions):
+    total = sum(fractions)
+    teste_frac = int(X.shape[0]*fractions[2]/total)
+    cv_frac = int(X.shape[0]*fractions[1]/total)
+    # print('Total, teste_frac e cv_frac', total, teste_frac, cv_frac)
+
+    X_teste = X[ : teste_frac]
+    X_cv = X[teste_frac : teste_frac+cv_frac]
+    X_treino = X[teste_frac+cv_frac : ]
+
+    return X_treino, X_cv, X_teste
+
 
 # retorna o custo e um vetor com os gradientes de cada parametro recebido
 def funcaoCusto(nn_params, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd): # lmbd -> lambda
@@ -82,10 +109,12 @@ def normalizar(X):
 
 def gradientesNumericos(J, theta):
     n = theta.size
-    e = 1e-4
+    e = 1e-7
     numgrad = np.zeros((n, 1))
     perturbacao = np.zeros((n, 1))
     for i in range(n):
+        if i>0 and i in [n//5, 2*n//5, 3*n//5, 4*n//5]:
+            print('Progresso do calculo de gradientes numericos:', 100*i//n+1, 'de 100')
         # print(i)
         perturbacao[i] = e
         # print(theta.shape, perturbacao.shape)
@@ -96,4 +125,73 @@ def gradientesNumericos(J, theta):
     return numgrad
 
 def otimizar(theta, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd):
-    return opt.fmin_cg(lambda x : funcaoCusto(x, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)[0], theta, fprime=(lambda x : funcaoCusto(x, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)[1]), maxiter=200)
+    ret = opt.fmin_cg(lambda x : funcaoCusto(x, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)[0], theta, fprime=(lambda x : funcaoCusto(x, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)[1]), maxiter=700)
+    return ret.reshape(ret.size, 1)
+
+# retorna 2 vetores: o primeiro mede a variacao do custo conforme se aumenta o grau das combinações, o segundo mede conforme se aumenta lambda
+# cada linha do vetor representa o resultado do experimento com um valor diferente de grau/lambda, e as colunas são:
+#   a primeira é o valor experimentado de grau/lambda, a segunda é o custo observado no conjunto de exemplos de treino e a terceira o custo observado no conjunto de exemplos de CV
+def AnaliseDeCombinacaoELambda(hidden_camada_tamanho, embeds, fw, spy, y, lmbd, fractions):
+    # separa os y's
+    (y, y_cv, _) = dividirExemplos(y, (6, 2, 2))
+    
+    # interface amigavel para o custo
+    J = lambda theta, size, X, yl : funcaoCusto(theta, size, hidden_camada_tamanho, X, yl, 0)[0]
+    
+    # custos por grau
+    print('Calculando custos por grau...')
+    for p in np.array(range(5))+1:
+        # ajustar entrada
+        X = np.concatenate((embeds, obterCombinacoes(fw, spy, p)), axis=1)
+        # print('Tamanho total de X:', X.shape)
+        (X, X_cv, _) = dividirExemplos(X, fractions)
+        # print('Tamanho de X final e de X_cv:', X.shape, X_cv.shape)
+        input_camada_tamanho = embeds.shape[1] + 2*p + sum(range(p))
+        # print('tamanho de X e da camada de input:', X.shape, input_camada_tamanho)
+
+        # pesos iniciais
+        theta1 = (np.random.rand(hidden_camada_tamanho, input_camada_tamanho+1) * (2 * 0.12)) - 0.12
+        theta2 = (np.random.rand(1, hidden_camada_tamanho+1) * (2 * 0.12)) - 0.12
+        nn_params = np.array([np.concatenate((theta1,theta2), axis=None)]).T
+        # print('Tamanhos de theta1, theta2 e nn_params:', theta1.shape, theta2.shape, nn_params.shape)
+
+        # otimizar
+        theta = otimizar(nn_params, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)
+        # print('Tamanho de theta final e seu tipo:', theta.shape, type(theta))
+        # salvar os custos
+        if p == 1:
+            custos_por_grau = np.array([[p, J(theta, input_camada_tamanho, X, y), J(theta, input_camada_tamanho, X_cv, y_cv)]])
+        else:
+            custos_por_grau = np.concatenate((custos_por_grau, np.array([[p, J(theta, input_camada_tamanho, X, y), J(theta, input_camada_tamanho, X_cv, y_cv)]])), axis=0)
+
+    # custos por lambda    
+    print('Calculando custos por lambda...')
+    # ajustar X
+    # ja pega o melhor resultado de p do teste anterior
+    # print(custos_por_grau)
+    min_p_i = np.argmin(custos_por_grau, axis=0)[2]
+    # print(np.argmin(custos_por_grau, axis=0), min_p_i, type(min_p_i))
+    min_p = int(custos_por_grau[min_p_i, 0])
+    # print(min_p)
+    X = np.concatenate((embeds, obterCombinacoes(fw, spy, min_p)), axis=1)
+    (X, X_cv, _) = dividirExemplos(X, fractions)
+    input_camada_tamanho = embeds.shape[1] + 2*min_p + sum(range(min_p))
+
+    # pesos iniciais
+    theta1 = (np.random.rand(hidden_camada_tamanho, input_camada_tamanho+1) * (2 * 0.12)) - 0.12
+    theta2 = (np.random.rand(1, hidden_camada_tamanho+1) * (2 * 0.12)) - 0.12
+    nn_params = np.array([np.concatenate((theta1,theta2), axis=None)]).T
+
+    # interface amigavel para o custo
+    J = lambda theta, x, yl : funcaoCusto(theta, input_camada_tamanho, hidden_camada_tamanho, x, yl, 0)[0]
+
+    l_set = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    for l in l_set:
+        # otimizar
+        theta = otimizar(nn_params, input_camada_tamanho, hidden_camada_tamanho, X, y, l)
+        # salvar os custos
+        if l == l_set[0]:
+            custos_por_lambda = np.array([[l, J(theta, X, y), J(theta, X_cv, y_cv)]])
+        else:
+            custos_por_lambda = np.concatenate((custos_por_lambda, np.array([[l, J(theta, X, y), J(theta, X_cv, y_cv)]])), axis=0)
+    return custos_por_grau, custos_por_lambda
--- script/rede_neural/test.py
+++ script/rede_neural/test.py
@@ -1,16 +1,17 @@
 from base import *
 # MODELO
 #   Rede neural em 3 camadas: 604 - 200 - 1. 
-m = 10
+m = 20
 grau = 2
-elementos_embed = 3
+elementos_embed = 10
+hidden_camada_tamanho = 20
+lmbd = 0.5
+
 input_camada_tamanho = 2 * elementos_embed + 2*grau + sum(range(grau))
-hidden_camada_tamanho = 2
-lmbd = 5
 
 # obter os exemplos de entrada
 X = gerarInputAleatorio(m, elementos_embed, grau)
-print(X.shape)
+print('Tamanho de X:', X.shape)
 # print('Input bruto:', X[0:2])
 # normalizar as entradas
 # X = normalizar(X)[0]
@@ -24,23 +25,31 @@ y = np.random.random((m, 1)).round()
 # inicializamos os pesos entre -0.12 e 0.12
 theta1 = (np.random.rand(hidden_camada_tamanho, input_camada_tamanho+1) * (2 * 0.12)) - 0.12
 theta2 = (np.random.rand(1, hidden_camada_tamanho+1) * (2 * 0.12)) - 0.12
-print('Thetas formados:', theta1, theta2)
+# print('Thetas formados:', theta1, theta2)
 
 # # função custo e gradientes
 nn_params = np.array([np.concatenate((theta1,theta2), axis=None)]).T
 # print(nn_params)
 (J, grad) = funcaoCusto(nn_params, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)
-print('Custo:', J)
+print('Custo inicial:', J)
 D1 = np.reshape(grad[:hidden_camada_tamanho*(input_camada_tamanho+1)], (hidden_camada_tamanho, input_camada_tamanho+1))
 D2 = np.reshape(grad[hidden_camada_tamanho*(input_camada_tamanho+1):], (1, hidden_camada_tamanho+1))
-print('Gradientes:', D1, D2)
+# print('Gradientes:', D1, D2)
 
 # gradient checking
 numgrad = gradientesNumericos(lambda t : funcaoCusto(t, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)[0], nn_params)
 numericalD1 = np.reshape(numgrad[:hidden_camada_tamanho*(input_camada_tamanho+1)], (hidden_camada_tamanho, input_camada_tamanho+1))
 numericalD2 = np.reshape(numgrad[hidden_camada_tamanho*(input_camada_tamanho+1):], (1, hidden_camada_tamanho+1))
-print('Gradientes numéricos:', numericalD1, numericalD2)
+# print('Gradientes numéricos:', numericalD1, numericalD2)
+print('Gradient check', np.mean(np.concatenate((D1-numericalD1, D2-numericalD2), axis=None)))
 
 # otimização
 otimizacao = otimizar(nn_params, input_camada_tamanho, hidden_camada_tamanho, X, y, lmbd)
-print(otimizacao)
+# print(otimizacao)
+
+input('Pressione algo para continuar')
+# analize de custos
+pos = elementos_embed*2
+# print('fwd e spy:', X[:, pos+1:pos+2], X[:, pos:pos+1])
+(custos_grau, custos_lambda) = AnaliseDeCombinacaoELambda(hidden_camada_tamanho, X[:, 0:pos], X[:, pos+1:pos+2], X[:, pos:pos+1], y, lmbd, (6,2,2))
+print('Custos por grau:', custos_grau, 'Custos por lambda:', custos_lambda)
